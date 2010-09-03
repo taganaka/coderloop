4f8a5c6340430dfcd4d940a6c649fdcebdf7fb82
taganaka <taganaka@gmail.com>
2010-09-04 01:22:47 +0200

    FriendsFinderGen.java Now outputs negative value also. You can pass a seed as argument

--- friendsfinder/FriendsFinderGen.java
+++ friendsfinder/FriendsFinderGen.java
@@ -9,21 +9,34 @@ public class FriendsFinderGen {
 	public static void main(String[] args) {
 		
 		if (args.length == 0){
-			System.err.println("usage: java FriendsFinderGen <number>");
+			System.err.println("usage: java FriendsFinderGen <number> [seed]");
 			System.exit(1);
 		}
 		
+		long seed;
+		
+		if (args.length >= 2){
+		    seed = Long.parseLong(args[1]);
+		}else{
+		    seed = System.currentTimeMillis();
+		}
+		
+		int high, low;
+		high = 500;
+		low  = -500;
+		
 		int number = Integer.parseInt(args[0]);
 		
-		long seed = System.currentTimeMillis();
+		
+		
 		
 		Random r = new Random(seed);
 		
 		for (int i = 1; i<=number; i++){
 			System.out.print(i + " ");
-			System.out.print(r.nextDouble() * r.nextInt(500));
+			System.out.printf("%.2f",r.nextDouble() * r.nextInt(high - low +1) + low);
 			System.out.print(" ");
-			System.out.print(r.nextDouble() * r.nextInt(500));
+			System.out.printf("%.2f",r.nextDouble() * r.nextInt(high - low +1) + low);
 			System.out.print("\n");
 		}
 	}
