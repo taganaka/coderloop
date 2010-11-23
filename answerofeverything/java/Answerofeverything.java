ed7003aa75a907bfc82c73fbf11a448412f46a10
taganaka <taganaka@gmail.com>
2010-11-23 13:38:29 +0100

    reformatted

--- answerofeverything/java/Answerofeverything.java
+++ answerofeverything/java/Answerofeverything.java
@@ -10,27 +10,25 @@ import java.util.Scanner;
 
 public class Answerofeverything {
 
-
     public static void main(String[] args) throws Exception{
-		
-		    if (args.length == 0){
-			    System.out.println("Unable to open file");
-			    System.exit(1);
-		    }
-		
-		    Scanner scanner = new Scanner(new File(args[0]));
-		
-		    while (scanner.hasNext()){
-                int d = scanner.nextInt();
-                if (d != 42){
-                    System.out.println(d);
-                }else{
-                    break;
-                }
-		    }
-		
-		    scanner.close();
-		
-    }
 
+        if (args.length == 0){
+            System.out.println("Unable to open file");
+            System.exit(1);
+        }
+
+        Scanner scanner = new Scanner(new File(args[0]));
+
+        while (scanner.hasNext()){
+            int d = scanner.nextInt();
+            if (d != 42){
+                System.out.println(d);
+            }else{
+                break;
+            }
+        }
+        
+        scanner.close();
+    
+    }
 }
